46f1e53889b05544dbd1068c4a6e02360c93d469
manooi <sirawit.mn@gmail.com>
2021-05-27 03:05:11 +0700

    Add mongoose and learn how to inspect network

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
 
+mongoose.
+  connect('mongodb://root:example@mongo:27017/?authSource=admin')
+  .then(() => {
+    console.log("Succesfully connected to DB");
+  })
+  .catch((e) => console.log(e));
+
+const app = express();
 
 app.get("/", (req, res) => {
   res.send("<h1>Hi There</h1>")
